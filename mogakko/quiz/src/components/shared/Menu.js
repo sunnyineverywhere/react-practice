0aa2285fefa684a288b61a97bce55282b70ed9cc
SEON UI LEE <sunnyinsummer@ewhain.net>
2022-01-21 09:48:41 +0900

    [project setting]

---
+++ mogakko/quiz/src/components/shared/Menu.js
@@ -0,0 +1,20 @@
+import styles from './Menu.module.css';
+import {Link} from 'react-router-dom';
+
+function Menu(){
+    return(
+        <div>
+            <div>
+                <Link to ="/">홈</Link>
+            </div>
+            <div>
+                <Link to ="/explore">탐색</Link>
+            </div>
+            <div>
+                <Link to ="/subscription">구독</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
